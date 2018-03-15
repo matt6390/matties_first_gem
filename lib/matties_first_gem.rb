a5b86a33cabd69727cd9c013984f1cb90e132df7
matt6390 <matthewstone639@gmail.com>
2018-03-15 18:21:33 -0500

    Initial commit

---
+++ lib/matties_first_gem.rb
@@ -0,0 +1,10 @@
+require "matties_first_gem/version"
+
+module MattiesFirstGem
+  class Matt
+    def shout
+      `say ooooooooooooo`
+      
+    end       
+  end
+end
---
+++ lib/matties_first_gem/version.rb
@@ -0,0 +1,3 @@
+module MattiesFirstGem
+  VERSION = "0.1.0"
+end
